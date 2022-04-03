815a9711a1b91cf73acacf2260eb887c27a6abb9
Praise Hart <hartpraise@gmail.com>
2022-04-04 00:10:13 +0100

    added sitemap file

--- pageController.js
+++ pageController.js
@@ -1,6 +1,11 @@
 const pageScraper = require('./pageScraper');
+const fs = require('fs');
+
 async function scrapeAll(browserInstance, req, res){
     const link = req.query.website;
+
+	// if path is given then a sitemap.xml file will be saved to the path after crawling successfully
+	const filePath = req.query.path;
 	let browser;
 	if (!link) {
 		console.log("No url provided.");
@@ -8,7 +13,21 @@ async function scrapeAll(browserInstance, req, res){
 	}else {
 		try{
 			browser = await browserInstance;
-			let response = await pageScraper.scraper(browser,link);	
+			let response = await pageScraper.scraper(browser,link);
+
+			//whether or not you want to create a sitemap.xml file
+			if (filePath) {
+				console.log(filePath);
+				try {
+					fs.writeFile(filePath + "/sitemap.xml", saveSitemap(response), (err) => {
+						if(err) return err;
+						return true;     
+					});
+				} catch (error) {
+					console.log(error);
+				}
+			}
+
 			res.status(200).send(response);
 		}
 		catch(err){
@@ -16,6 +35,15 @@ async function scrapeAll(browserInstance, req, res){
 		}
 	}
 	
+}
+
+let saveSitemap = (links) => {
+    var map = '<?xml version="1.0" encoding="UTF-8"?>\n<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n';
+    links.forEach(link => {
+        map += '<url><loc>' + link.url +'</loc><priority>0.5</priority></url>\n';
+    });
+    map += '</urlset>';
+    return map;
 }
 
 module.exports = (browserInstance, req, res) => scrapeAll(browserInstance, req, res)
